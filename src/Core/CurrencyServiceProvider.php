95ee6c6332cab3e1dd2e5bad52d684b87b432d93
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2019-03-01 17:40:50 +0200

    updates

--- src/Core/CurrencyServiceProvider.php
+++ src/Core/CurrencyServiceProvider.php
@@ -5,6 +5,10 @@ namespace Abdelrahman_badr\CurrencyRates\Core;
 use Illuminate\Support\ServiceProvider;
 
 use Abdelrahman_badr\CurrencyRates\Services\CurrencyService;
+use Abdelrahman_badr\CurrencyRates\Providers\ExchangeRatesApiProvider;
+use Abdelrahman_badr\CurrencyRates\Mappers\CurrencyMapper;
+use Abdelrahman_badr\CurrencyRates\Services\Http\GuzzleHttpAdapter;
+
 
 class CurrencyServiceProvider extends ServiceProvider
 {
@@ -25,13 +29,7 @@ class CurrencyServiceProvider extends ServiceProvider
      */
     public function register()
     {
-
-
-        $this->app->singleton('Abdelrahman_badr\CurrencyRates\Core\Contracts\CurrencyMapperInterface', 'Abdelrahman_badr\CurrencyRates\Mappers\CurrencyMapper');
-        $this->app->singleton('Abdelrahman_badr\CurrencyRates\Core\Contracts\CurrencyProviderInterface', 'Abdelrahman_badr\CurrencyRates\Providers\ExchangeRatesApiProvider');
-        $this->app->singleton('Abdelrahman_badr\CurrencyRates\Core\Contracts\CurrencyServiceInterface', 'Abdelrahman_badr\CurrencyRates\Services\CurrencyService');
-        $this->app->singleton('Abdelrahman_badr\CurrencyRates\Core\Contracts\HttpAdapterInterface', 'Abdelrahman_badr\CurrencyRates\Services\Http\GuzzleHttpAdapter');
-        $this->app->instance('CurrencyService', CurrencyService::Class);
+        $this->app->instance('CurrencyService', new CurrencyService(new ExchangeRatesApiProvider(), new CurrencyMapper(), new GuzzleHttpAdapter()));
     }
 
 }
